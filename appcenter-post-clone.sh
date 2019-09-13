8901dde04dc42cf79040c9873d1cbded9d35d18a
Cesar Sturion <csturion@gmail.com>
2019-09-13 14:08:26 -0300

    alterando para baixar a versao do sonar-scanner do macOS

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -5,10 +5,10 @@ npm install -g appcenter-cli
 
 # Download and unzip the Scanner for your CI
 export SONAR_SCANNER_VERSION=4.0.0.1744
-export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux
+export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-macosx
 rm -rf $SONAR_SCANNER_HOME
 mkdir -p $SONAR_SCANNER_HOME
-curl -sSLo $HOME/.sonar/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-linux.zip
+curl -sSLo $HOME/.sonar/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-macosx.zip
 unzip $HOME/.sonar/sonar-scanner.zip -d $HOME/.sonar/
 rm $HOME/.sonar/sonar-scanner.zip
 export PATH=$SONAR_SCANNER_HOME/bin:$PATH
